c1c460a83663ca38a930009f7030dd1b20b5c022
masukomi <masukomi@masukomi.org>
2012-06-19 14:29:54 -0400

    added bin/system_setup.sh

---
+++ bin/system_setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+# to see changed lines use git lg -p
+git config --global alias.st "status -sb"
