5388e55c9abbd5fa4feb073297c36d62ef287cbf
masukomi <masukomi@masukomi.org>
2013-02-21 16:55:52 -0500

    added brew commands to system_setup.sh

--- bin/system_setup.sh
+++ bin/system_setup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/sh
 
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+#git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 # to see changed lines use git lg -p
-git config --global alias.st "status -sb"
+#git config --global alias.st "status -sb"
+
+brew install ctags
+brew install dwdiff
+brew install gettext
+brew install imagemagick
+brew install readline
+brew install ossp-uuid
