0caa7e10c1c03c42d3a39e9528792748b1efe406
Kay Rhodes <krhodes@rbmtechnologies.com>
2013-05-07 08:32:35 -0400

    updated system setup to work with new vim submodules

--- bin/system_setup.sh
+++ bin/system_setup.sh
@@ -13,28 +13,14 @@ brew install readline
 brew install ossp-uuid
 brew install tig
 
-# vim plugins
-# we're cding in and calling git update after clone
-# so that we can run it and have it be useful even 
-# if the repo has already been cloned.
-mkdir -p ~/.vim/bundle
-cd ~/.vim/bundle
-git clone git://github.com/godlygeek/tabular.git
-cd tabular
-git update
-cd ../
-git clone https://github.com/kchmck/vim-coffee-script.git
-cd vim-coffee-script
-git update
-cd ../
-git clone git://github.com/tpope/vim-endwise.git
-cd vim-endwise
-git update
-cd ../
-git clone git://github.com/tpope/vim-fugitive.git
-cd vim-fugitive
-git update 
-cd ../
+mkdir ~/workspace
+cd workspace
+git checkout ssh://masukomi@masukomi.org/home/masukomi/home_dir_configs.git
+cd home_dir_configs
+git submodule init
+git submodule update
+ln -s ~/workspace/home_dir_configs/.vim/bundle ~/.vim/bundle
+
 
 # command-t should already be installed via a vimball stored in the git repo
 # https://wincent.com/products/command-t
