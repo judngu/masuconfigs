943db44676120c2a76406f76463df0abb663c769
masukomi <masukomi@masukomi.org>
2013-10-29 22:55:55 -0400

    added ccl setup commands and instructions

--- bin/system_setup.sh
+++ bin/system_setup.sh
@@ -57,7 +57,11 @@ git submodule update
 ln -s ~/workspace/home_dir_configs/.vim/bundle ~/.vim/bundle
 ln -s ~/workspace/home_dir_configs/.emacs ~/.emacs
 ln -s ~/workspace/home_dir_configs/.emacs.d ~/.emacs.d
-
+mkdir -p /usr/local/src
+ln -s /usr/local/src/ccl /Applications/ccl
+ln -s /Applications/ccl/scripts/ccl ~/bin/ccl
 
 # command-t should already be installed via a vimball stored in the git repo
 # https://wincent.com/products/command-t
+
+
