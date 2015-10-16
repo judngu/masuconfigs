7bd6cb53c54b16cd9669f9b8aec3c1444cc14bda
masukomi <masukomi@masukomi.org>
2015-10-16 07:59:29 -0400

    [emacs] installed spacemacs
    https://github.com/syl20bnr/spacemacs
    In theory it's a better version of emacs + evil

--- bin/system_setup.sh
+++ bin/system_setup.sh
@@ -55,6 +55,15 @@ brew install gpg
 brew install tig
 brew install pgcli
 
+### for spacemacs https://github.com/syl20bnr/spacemacs
+brew tap railwaycat/homebrew-emacsmacport
+brew install emacs-mac --with-spacemacs-icon
+### end spacemacs
+
+brew linkapps
+# links the experimental Emacs.app (from spacemacs) to /Applications
+# along with anything else that needs to be linked there
+# could just do brew linkapps emacs-mac 
 
 mkdir ~/workspace
 cd workspace
@@ -65,8 +74,14 @@ git submodule update
 ln -s ~/workspace/home_dir_configs/.vim/bundle ~/.vim/bundle
 ln -s ~/workspace/home_dir_configs/slime.vim ~/slime.vim
 ln -s ~/workspace/home_dir_configs/.emacs ~/.emacs
-ln -s ~/workspace/home_dir_configs/.emacs.d ~/.emacs.d
-
+### spacemacs
+#ln -s ~/workspace/home_dir_configs/.emacs.d ~/.emacs.d
+if [ -e "~/.emacs.d" ]; then
+	mv ~/.emacs.d ~/.emacs.bak.was.dot.d
+fi
+git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
+ln -s ~/workspace/home_dir_configs/.spacemacs ~/.spacemacs
+### end spacemacs
 
 ln -s ~/workspace/home_dir_configs/.vimrc ~/.vimrc
 ln -s ~/workspace/home_dir_configs/.vim ~/.vim
