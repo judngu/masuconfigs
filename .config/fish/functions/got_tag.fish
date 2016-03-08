e9ce488da4c08511bc1466c001cf1b2a07b93909
Kay Rhodes <krhodes@rbmtechnologies.com>
2016-03-08 17:44:46 -0500

    [fish] added got_tag function

--- .config/fish/functions/got_tag.fish
+++ .config/fish/functions/got_tag.fish
@@ -1,5 +1,5 @@
 function got_tag
-	set -l tag (git fetch -q upstream; and git tag | grep $argv)
+	set -l TAG (git fetch -q upstream; and git tag | grep $argv)
 	if [ "$TAG" = "" ]
 		echo "nope"
 	else
