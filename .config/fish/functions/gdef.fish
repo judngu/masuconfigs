c3ff7fc713e4b7933d1f9d5b4f2c7c0129170396
Kay Rhodes <krhodes@rbmtechnologies.com>
2016-11-29 09:47:26 -0500

    [fish] corrected gdef function

--- .config/fish/functions/gdef.fish
+++ .config/fish/functions/gdef.fish
@@ -1,7 +1,3 @@
 function gdef --description="grep for ruby function definitions"
-	grep -r $argv * | grep def | grep -v "tags:"
-	# todo figure out how to make it support 
-	# gdef foo in/this/folder/
-	# and have it result in 
-	# grep -r foo in/this/folder/* | grep def | grep -v "tags:"
+	grep -r $argv | grep def | grep -v "tags:"
 end
